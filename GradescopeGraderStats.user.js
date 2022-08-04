035b41b7c3b08633aaf8fafa625454c8a00d1a9f
David Harris <david@gtdavid.com>
2022-08-04 18:04:54 -0400

    Add per-grader frequency charts
    Clicking on each grader summary row opens a chart showing score frequency.

--- GradescopeGraderStats.user.js
+++ GradescopeGraderStats.user.js
@@ -26,14 +26,16 @@
 // @name            Gradescope Grader Stats
 // @namespace       https://greasyfork.org/en/users/238426-drharris
 // @description     Displays per-grader statistics on submissions in Gradescope
-// @version         1.0.0
+// @version         1.1.0
 // @author          drharris
 // @copyright       2022, drharris (https://greasyfork.org/en/users/238426-drharris)
 // @license         MIT
-// @homepage        https://github.com/drharris/gradescope_grader_stats
-// @supportURL      https://github.com/drharris/gradescope_grader_stats/issues
+// @homepage        https://greasyfork.org/en/scripts/423266-sample-userscript-template
+// @supportURL      https://greasyfork.org/en/scripts/423266-sample-userscript-template/feedback
 // @match           https://www.gradescope.com/courses/*/questions/*/submissions
 // @require         https://unpkg.com/mathjs@7.0.0/dist/math.min.js
+// @require         https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
+// @require         https://cdn.jsdelivr.net/npm/chart.js@3.9.1/dist/chart.min.js
 // @grant           GM_getValue
 // @grant           GM_setValue
 // @grant           GM_deleteValue
@@ -51,35 +53,54 @@
 // @grant           GM_openInTab
 // @grant           GM_notification
 // ==/UserScript==
- 
+
 // ==OpenUserScript==
 // @author          drharris
 // @collaborator    drharris
 // ==/OpenUserScript==
- 
+
 /* jshint esversion: 6 */
 
 $(document).ready(function () {
-    if(!$('#question_submissions_wrapper .dataTable >tbody >tr')) return;
+    if (!$('#question_submissions_wrapper .dataTable >tbody >tr')) return;
     var dict = {};
-    $('#question_submissions_wrapper .dataTable >tbody >tr').each(function(index, tr) {
-        if(!(tr.childNodes[2].textContent in dict)) {
-            dict[tr.childNodes[2].textContent] = [];
-        }
-        dict[tr.childNodes[2].textContent].push(tr.childNodes[4].textContent);
+    var uniqueScores = [];
+    // parse grade data
+    $('#question_submissions_wrapper .dataTable >tbody >tr').each(function (index, tr) {
+        var grader = tr.childNodes[2].textContent;
+        var score = tr.childNodes[4].textContent;
+        if (!uniqueScores.includes(score)) { uniqueScores.push(score); }
+        if (!(grader in dict)) { dict[grader] = []; }
+        dict[grader].push(score);
     });
-    var table = $('<table>').addClass('table').css("margin", "3em 0 0 6em").css("width", "80%").appendTo($('#question_submissions_filter'));
-    var thead = $('<thead>').appendTo(table);
-    var tr = $('<tr>').appendTo(thead);
+    uniqueScores.sort((a, b) => (parseInt(a) - parseInt(b)));
+    // build new table
+    var table = $('<table>').addClass('table').css("margin", "3em auto 0").css("width", "80%").appendTo($('#question_submissions_filter'));
+    var tr = $('<tr>').appendTo($('<thead>').appendTo(table));
     tr.append($('<th>Grader</th><th>Count</th><th>Mean</th><th>StDev</th>'));
     var tbody = $('<tbody>').addClass('collapse').attr('id', 'gsgsTbody').appendTo(table);
-    for(var ta in dict) {
-        if(ta == '') continue;
-        var row = $('<tr>').css("height", "32px").appendTo(tbody);
+    for (var ta in dict) {
+        if (ta == '') continue;
+        // make data summary row
+        var row = $('<tr>').addClass('gsgsRow').css("cursor", "pointer").css("height", "32px").appendTo(tbody);
         row.append($('<td>').text(ta))
         row.append($('<td>').text(dict[ta].length));
-        row.append($('<td>').text(math.round(math.mean(dict[ta]), 3)))
-        row.append($('<td>').text(math.round(math.std(dict[ta]), 3)))
+        row.append($('<td>').text(math.round(math.mean(dict[ta]), 2)))
+        row.append($('<td>').text(math.round(math.std(dict[ta]), 2)))
+        // make chart row
+        var detailRow = $('<tr>').addClass('gsgsRowDetail').appendTo(tbody);
+        var detailCell = $('<td colspan=4></td>').appendTo(detailRow);
+        var chartContainer = $('<div style="display:inline-block;position:relative;width:100%;height:200px;padding-left:2em;padding-right:2em"></div>').appendTo(detailCell);
+        let scoreCount = dict[ta].reduce(function ( c, v ) { return ( c[v] ? ++c[v] : (c[v] = 1), c ); }, {});
+        new Chart($('<canvas></canvas>').appendTo(chartContainer), {
+            type: 'bar',
+            data: { labels: uniqueScores, datasets: [ { label:ta, data: scoreCount, backgroundColor: 'rgba(92, 60, 146, 0.5)' } ] },
+            options: { plugins: { legend: { display: false, } }, responsive: true, maintainAspectRatio:false }
+        });
+        detailRow.toggle();
     }
+    // add chart collapse ability
+    $('tr.gsgsRow').click(function () {
+        $(this).nextUntil('tr.gsgsRow').slideToggle(100);
+    });
 });
- 
